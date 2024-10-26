bbaa57059edfabc74a6ff0ec74f74fa8a6580181
ScootKampfer <125150185+ScootKampfer@users.noreply.github.com>
2024-10-26 12:51:17 -0400

    added password function for admin privileges (security to come later)

--- menu.py
+++ menu.py
@@ -9,6 +9,14 @@ def writemenu():
         FutureMenu.write(str(menu)) 
         FutureMenu.close() 
 
+def security():
+     
+     attempt = input("Please input your password to gain access to admin privileges: ")
+     if attempt != password:
+
+          print('Your password was incorrect')
+          exit()
+
 def switch(ToDo):
 
     if ToDo == "view menu":
@@ -17,6 +25,7 @@ def switch(ToDo):
     
     elif ToDo == "add item":
 
+        security()
         NewMenuItem = input("What is the name of the new menu item? ")
         NewMenuPrice = "$" + format(float(input(f"And what is the price of a {NewMenuItem} in dollars? ")), ".2f")
         menu[NewMenuItem] = NewMenuPrice
@@ -25,20 +34,33 @@ def switch(ToDo):
     
     elif ToDo == "remove item":
 
+        security()
         Removal = input("What is the name of the menu item you wish to be removed? ")
         del menu[Removal]
         writemenu()
         return menu
     
-    elif ToDo == "save and quit":
+    elif ToDo == "Change password":
+         
+         security()
+         
+         global password
+         password = input("Please enter the new password")
+         message = "Your password has now been changed"
+         return message
+    
+    elif ToDo == "quit":
 
         writemenu()
-        print("Your menu has been saved")
+        print("Thank you for using our software.")
         exit()
         return menu
+
+password = "6942"
 
 while True:
     
     menu = reading()
     action = input("Enter action to do here: ")
     print(switch(action))
+
