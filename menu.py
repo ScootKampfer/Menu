d9bfaad00a82699e9b4dd94310e45b2f1ffff6e1
ScootKampfer <125150185+ScootKampfer@users.noreply.github.com>
2024-10-29 18:49:29 -0400

    added so you don't need to login every time you did something, now only needing to log in once per session. Also added tabs to make this more readable in the terminal (ocd effect).

--- menu.py
+++ menu.py
@@ -1,3 +1,6 @@
+import time
+login = 0
+
 def menureading():
     
     PreviousMenu = eval(open('menu.txt', 'r').read())
@@ -12,88 +15,122 @@ def writemenu():
 def security():
      
      password = open("C:\\Users\\gaben\\OneDrive\\Documents\\very fun number sequences.txt", 'r').read()
-     attempt = input("Please input your password to gain access to admin privileges: ")
+     attempt = input("\nPlease input your password to gain access to admin privileges: \n\n")
      if attempt != password:
 
-          print('Your password was incorrect')
-          print("Access terminated!")
+          print('\nYour password was incorrect.')
+          print("\nAccess terminated!")
+          time.sleep(1)
+          exit()
+
+     global login 
+     login = 1
+
+def lock():
+
+     if login != 1:
+
+          print("\nAccess denied!\n")
+          time.sleep(1)
           exit()
 
 def switch(ToDo):
 
     if ToDo == "view menu":
 
-        return menu
+     
+        tabbedmenu = "\n" + str(menu)
+        return tabbedmenu
     
     elif ToDo == "help":
          
-         message = "\n The available commands are as follows: \n \n - view menu \n - quit \n"
+         message = "\nThe available commands are as follows: \n \n - view menu \n - quit \n\nIf you need to access admin controls, you need to login with the login command."
          return message
 
     elif ToDo == "admin help":
          
-         security()
-         message = "\n The available commands are as follows: \n \n - view menu \n - add item \n - clear \n - remove item \n - change password \n - quit \n"
+         lock()
+         message = "\n The available commands are as follows: \n \n - view menu \n - add item \n - clear \n - remove item \n - change password \n - quit"
          return message
 
     elif ToDo == "clear":
          
-         security()
-         areyousure = input("are you sure you want to clear the entire menu? type CLEAR if you are sure you want to clear?")
+         lock()
+         areyousure = input("\nAre you sure you want to clear the entire menu? Type CLEAR if you are sure you want to clear: \n\n")
          
          if areyousure == "CLEAR":
               
               menu.clear()
               writemenu()
-              message = "The menu has been cleared."
+              message = "\nThe menu has been cleared."
               return message
          
          else:
               
-              message = "Action canceled."
+              message = "\nAction canceled."
               return message
 
     elif ToDo == "add item":
 
-        security()
-        NewMenuItem = input("What is the name of the new menu item? ")
-        NewMenuPrice = "$" + format(float(input(f"And what is the price of a {NewMenuItem} in dollars? ")), ".2f")
+        lock()
+        NewMenuItem = input("\nWhat is the name of the new menu item?\n\n")
+        NewMenuPrice = "$" + format(float(input(f"\nAnd what is the price of a {NewMenuItem} in dollars?\n\n")), ".2f")
         menu[NewMenuItem] = NewMenuPrice
         writemenu()
-        return menu
+        tabbedmenu = "\n" + str(menu)
+        return tabbedmenu
     
     elif ToDo == "remove item":
 
-        security()
-        Removal = input("What is the name of the menu item you wish to be removed? ")
-        del menu[Removal]
-        writemenu()
-        return menu
+        lock()
+        Removal = input("\nWhat is the name of the menu item you wish to be removed?\n\n")
+        
+        try:
+
+          del menu[Removal]
+          writemenu()
+          tabbedmenu = "\n" + str(menu)
+          return tabbedmenu
+
+        except:
+
+          message = "\nThis menu item does not exist!"
+          return message
+
     
     elif ToDo == "change password":
          
-         security()
+         lock()
          
          global password
-         password = input("Please enter the new password: ")
+         password = input("\nPlease enter the new password:\n\n")
          Changes = open("C:\\Users\\gaben\\OneDrive\\Documents\\very fun number sequences.txt", "wt")
          Changes.write(str(password))
          Changes.close()
-         message = "Your password has now been changed"
+         message = "\nYour password has now been changed."
          return message
 
     elif ToDo == "quit":
-
+     
+        lock()
         writemenu()
-        print("Thank you for using our software. Your menu has been saved.")
+        print("\nThank you for using our software. Your menu has been saved.\n")
+        global login
+        login = 0
+        time.sleep(1)
         exit()
         return menu
+        
+    elif ToDo == "login":
 
-print("type help for all available commands \n")
+          security()
+          message = "\nYou have been successfully logged in"
+          return message
+
+print("\nType help for all available commands.")
 
 while True:
     
     menu = menureading()
-    action = input("Enter action to do here: ")
+    action = input("\nEnter action to do here: \n\n")
     print(switch(action))
-    
